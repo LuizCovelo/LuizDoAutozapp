f0f5355ab4d3dc0646695df70c962028e30b3890
LuizCovelo <214908977+LuizCovelo@users.noreply.github.com>
2026-02-03 20:46:02 -0500

    feat: implement functional PWA with persuasive install and notification modals

--- sw.js
+++ sw.js
@@ -1,17 +1,49 @@
-const CACHE_NAME = 'luiz-autozapp-v1';
+const CACHE_NAME = 'luiz-autozapp-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/favicon.png',
+  '/logo.jpg'
+];
 
 self.addEventListener('install', event => {
-  console.log('[SW] Instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  console.log('[SW] Ativado');
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          if (event.request.method === 'GET') {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      });
+    }).catch(() => {
+      if (event.request.mode === 'navigate') {
+        return caches.match('/');
+      }
+    })
   );
 });
